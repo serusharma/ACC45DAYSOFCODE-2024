9fb26aa1043528db69611c702ff7e1462a32a3e0
serusharma <sharmasachin80505@gmail.com>
2024-09-29 23:33:27 +0530

    Add files via upload

---
+++ javaguthub Day-01.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int a = 2;
+	    int b = 7;
+	    int sum = a + b;
+	    System.out.println(sum);
+		// your code goes here
+
+	}
+}
